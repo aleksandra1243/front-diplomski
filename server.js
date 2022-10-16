0912bc163109b60a520b454b0b4cea4482e555e3
Aleksandra <aleksandra.markovic.1812@gmail.com>
2022-10-16 19:40:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('hero.html', {root: 'dist/angular-app-heroku/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3001);
